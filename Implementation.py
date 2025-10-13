b90c35896f74c4b09f94cb9a06712318b2858798
MatthewRisplerUTS <matthew.d.rispler@student.uts.edu.au>
2025-10-13 13:57:10 +1100

    Added handling for the number and identifier tokens

--- Implementation.py
+++ Implementation.py
@@ -6,3 +6,25 @@ def lexer(input):
 
     for i in range(n):
         char = input[i]
+
+        if char.isdigit():
+            number_str = char
+            i += 1
+
+            while input[i].isdigit() and i < n:
+                number_str += input[i]
+                i += 1
+            
+            tokens.append(('NUMBER', int(number_str)))
+            continue
+        
+        if char.isalpha():
+            identifier_str = char
+            i += 1
+
+            while input[i].isalpha() and i < n:
+                identifier_str += input[i]
+                i += 1
+            
+            tokens.append(('IDENTIFIER', identifier_str))
+            continue
