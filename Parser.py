d2471baed809ab28d00d3b51d874e78acf1ffc9c
MatthewRisplerUTS <matthew.d.rispler@student.uts.edu.au>
2025-10-24 18:36:31 +1100

    Created parser stack and main parsing loop. Added handling for if the top parser stack element is non terminal. Followed standard LL(1) parsing algorithm, checking if the current "top symbol" and "next token" were valid according to the parse_table, then appending the reversed parse table token's symbols to the parser stack. Added error handling for unexpected tokens.

--- Parser.py
+++ Parser.py
@@ -3,3 +3,17 @@ from Lexer import lexer
 parse_table = {}
 
 def parser(tokens, parse_table):
+    parser_stack = ['$', '<program>']
+    position = 0
+    next_token = tokens[position][0]
+
+    while len(parser_stack) != 0:
+        top_symbol = parser_stack.pop()
+
+        if type(top_symbol) == str and top_symbol.startswith('<'):
+            if (top_symbol, next_token) in parse_table:
+                non_reversed_pt = parse_table(top_symbol, next_token)
+                for element in reversed(non_reversed_pt):
+                    parser_stack.append(element)
+            else:
+                raise SyntaxError("Unexpected lexer token")
