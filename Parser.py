e6bbacfb8f545d44146ba69718a4003e84115761
MatthewRisplerUTS <matthew.d.rispler@student.uts.edu.au>
2025-10-25 13:25:41 +1100

    Added handling for when the top parser stack element is terminal. If the current next_token matches the  element, the element is popped from the stack and then the token is advanced. Added handling for when the stack reaches $. Added error handling placeholders and changed element variable name to symbol.

--- Parser.py
+++ Parser.py
@@ -13,7 +13,20 @@ def parser(tokens, parse_table):
         if type(top_symbol) == str and top_symbol.startswith('<'):
             if (top_symbol, next_token) in parse_table:
                 non_reversed_pt = parse_table(top_symbol, next_token)
-                for element in reversed(non_reversed_pt):
-                    parser_stack.append(element)
+                for symbol in reversed(non_reversed_pt):
+                    parser_stack.append(symbol)
             else:
                 raise SyntaxError("Unexpected lexer token")
+            
+        if top_symbol.startswith('<') == False:
+            if top_symbol == next_token:
+                parser_stack.pop()
+                position += 1
+                next_token = tokens[position][0]
+            else:
+                raise SyntaxError("Error")
+        
+    if next_token == '$':
+        print("Complete")
+    else:
+        raise SyntaxError("Error")
